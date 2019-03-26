2b778089bb069e557eaa52ba9eb4ad73b0c67660
Raúl Moreno <raulmoreno899@gmail.com>
2019-03-26 01:08:06 -0600

    Agregando btn Agregar y algunos estilos iniciales
    se añade la cabecera, el espacio de trabajo y un botón con el que
    agregaremos elementos a la base de datos más adelante

--- index.php
+++ index.php
@@ -2,11 +2,33 @@
 <html lang="es">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0"/>
+	<meta http-equiv="expires" content="0"/>
+	<meta name="robots" content="inicio"/>
 	<title>GAMES - Inicio</title>
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<link rel="icon" type="image/png" href="./images/icono.png" />
+	<link rel="stylesheet" type="text/css" href="css/colores.css">
+	<link rel="stylesheet" type="text/css" href="css/buttons.css">
+	<link rel="stylesheet" type="text/css" href="css/general.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
 </head>
 <body>
-
+	<div id="menu">
+		<div class="menu-logo">
+			My Games
+		</div>
+	</div>
+	<div id="board">
+		
+	</div>
+	<footer>
+		Todos los derechos reservados @ Raul Moreno
+	</footer>
+	<div id="controls">
+		<button id="btn-add" class="btn-circle">
+			<img src="images/add.png">
+		</button>
+	</div>
 </body>
 </html>
