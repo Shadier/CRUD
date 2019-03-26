b6422b97c5fc70d840f38eb8a2af7efa0f238953
Raúl Moreno <raulmoreno899@gmail.com>
2019-03-25 23:53:42 -0600

    Actualizacion del icono

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>GAMES - Inicio</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="icon" type="image/png" href="./images/icono.png" />
+</head>
+<body>
+
+</body>
+</html>
